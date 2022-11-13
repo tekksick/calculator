28d0ebceaba3af56279aad0ce1d0bc9d20c80667
tekksick <117775726+tekksick@users.noreply.github.com>
2022-11-13 22:42:45 +0530

    Add files via upload

--- calc.cpp
+++ calc.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
-int fact(int a)
+
+
+long long fact(long long a)
 {
-  int b=1,c=1;
+  long long b=1;
+  long long c=1;
     while(b<=a)
     {
         c=c*b;
@@ -10,13 +13,9 @@ int fact(int a)
     }
    return c;
 }
-int sign(int a)
-{
-    int c;
-    if(a%4==0 || a%4==1) c=1;
-    else c=-1;
-    return c;
-}
+
+
+
 double exp(double a,int b)
 {
     double c=1;
@@ -28,6 +27,9 @@ double exp(double a,int b)
     }
     return c;
 }
+
+
+
 double fun(int b)
 {
     int a=1;
@@ -41,7 +43,12 @@ double fun(int b)
     c=c/b;
     return c;
 }
+
+
+
 int main()
+
+
 {
     char choice='y';
     int num;
@@ -62,6 +69,11 @@ int main()
    if(num==4) c=a/b;
     cout<<"The result is "<<c<<endl;
    }
+
+
+   //exp
+
+
    else if(num==5)
    {
     int i,k=1;
@@ -84,63 +96,114 @@ int main()
     cout<<"The result is "<<exp(a,b)<<endl;
     }
    }
+
+
+   //trigo
+
+
     else if(num==6)
    {
-    int i,x;
+    int i,x,sign;
     c=0;
     cout<<"For sin press 1"<<endl<<"For cos press 2"<<endl<<"For tan press 3"<<endl<<"For sec press 4"<<endl<<"For cosec press 5"<<endl<<"For cot press 6"<<endl;
     cin>>i;
-    cout<<"For DEG press 1"<<endl<<"For RAD press 2"<<endl;
-    //ask if user wants to give input as a multiple of pi
+    cout<<"To input in DEG press 1 and in RAD press 2"<<endl;
     cin>>b;
     cout<<"Enter the value"<<endl;
     cin>>a;
     if(b==1) a=(a*pi)/180;
-    if(i==1) {x=1; while(x<14) {c+=sign(x)*exp(a,x)/fact(x);      x+=2;} }
-    if(i==2) {x=0; while(x<14) {c+=sign(x)*exp(a,x)/fact(x);      x+=2;} }
-    if(i==3) c=a+exp(a,3)/3+exp(a,5)*2/15+exp(a,7)*17/315;
-    if(i==4) c=1+exp(a,2)/2+exp(a,4)*5/24+exp(a,6)*61/720;
-    if(i==5) c=1/a+a/6+7*exp(a,3)/360+exp(a,5)*31/15120;
-    if(i==6) c=1/a-a/3+exp(a,3)/45-exp(a,5)*2/945;
-     cout<<"The value is "<<c<<endl;
+     x=1,sign=1;
+  double sin=0;
+  //p=a%(pi/2);
+  while(x<14) {sin+=sign*exp(a,x)/fact(x);      x+=2;  sign*=-1;}
+  x=2,sign=-1;
+  double cos=1;
+  while(x<15) {cos+=sign*exp(a,x)/fact(x);      x+=2;  sign*=-1;}
+  if(cos<0.05 && cos>-0.005) cos=0;
+  if(sin<0.005 && sin>-0.005) sin=0;
+ // if(cos<-1) cos=-1;
+  //if(cos>1 ||)
+  //cout<<"sin " << sin<<"cos "<< cos;
+   if(i==1){ c=sin;  cout<<"The value is "<<c<<endl;}
+   else if(i==2) {c=cos;  cout<<"The value is "<<c<<endl;}
+   else if(i==3 && cos!=0) {c=sin/cos; if(c=-0) c=0; cout<<"The value is "<<c<<endl;}
+   else if(i==4 && cos!=0) {c=1/cos;   cout<<"The value is "<<c<<endl;}
+   else if(i==5 && sin!=0) {c=1/sin;  cout<<"The value is "<<c<<endl;}
+   else if(i==6 && sin!=0) {c=cos/sin;  cout<<"The value is "<<c<<endl;}
+   else cout<<"Invalid input"<<endl;
    }
+
+
+   //inv trigo
+
+
    else if(num==7)
    {
-    int i,k;
-    c=0;
+    int i,k,sign=1;
+    b=1;
     cout<<"For inv of sin press 1"<<endl<<"For inv of cos press 2"<<endl<<"For inv of tan press 3"<<endl<<"For inv of sec press 4"<<endl<<"For inv of cosec press 5"<<endl<<"For inv of cot press 6"<<endl;
     cin>>i;
     cout<<"Enter the value"<<endl;
     cin>>a;
-    if(i==1) {b=1; while(b<60000) {c+=exp(a,b)*fun(b);  b+=2;}};
-    if(i==2) {b=1; while(b<60000) {c+=exp(a,b)*fun(b);  b+=2;} c=pi/2-c;};
-    if(i==3) {b=1; while(b<60000) {c+=exp(a,b)*sign(b)/b;  b+=2;}};
-    if(i==4) {b=1; while(b<60000) {c+=fun(b)/exp(a,b);  b+=2;} c=pi/2-c;};
-    if(i==5) {b=1; while(b<60000) {c+=fun(b)/exp(a,b);  b+=2;}};
-    if(i==6) {b=1; while(b<60000) {c+=exp(a,b)*sign(b)/b;  b+=2;} c=pi/2-c;};
+    if(i==1) {while(b<60000) {c+=exp(a,b)*fun(b);  b+=2;}}
+    if(i==2) {while(b<60000) {c+=exp(a,b)*fun(b);  b+=2;} c=pi/2-c;}
+
+//tan
+
+    if(i==3) 
+    {
+      if(a<1 && a>-1) {while(b<60000) {c+=exp(a,b)*sign/b;  b+=2;   sign*=-1;}}
+     else if(a>=1) {c=pi/2; sign=-1; while(b<60000) {c+=exp(a,-1*b)*sign/b;  b+=2;  sign*=-1;}}
+     else{c=-1*pi/2; sign=-1; while(b<60000) {c+=exp(a,-1*b)*sign/b;  b+=2;  sign*=-1;}}
+    }
+
+     if(i==4) {while(b<60000) {c+=fun(b)/exp(a,b);  b+=2;} c=pi/2-c;}
+     if(i==5) {while(b<60000) {c+=fun(b)/exp(a,b);  b+=2;}}
+     if(i==6) 
+    {
+    {
+     if(a<1 && a>-1) {while(b<60000) {c+=exp(a,b)*sign/b;  b+=2;   sign*=-1;}}
+     else if(a>=1) {c=pi/2; sign=-1; while(b<60000) {c+=exp(a,-1*b)*sign/b;  b+=2;  sign*=-1;}}
+     else{c=-1*pi/2; sign=-1; while(b<60000) {c+=exp(a,-1*b)*sign/b;  b+=2;  sign*=-1;}}
+     c=pi/2-c;
+    }
+    }
     cout<<"For result in DEG press 1 and for RAD press 2"<<endl;
     cin>>k;
     if(k==1) c=c*180/pi;
     cout<<"The result is "<<c<<endl;
    }
+  
+
+  //factorial
+
+
    else if(num==8)
    {
     cout<<"Enter the value "<<endl;
     cin>>a;
      cout<<"The result is "<<fact(a)<<endl;
    }
-   else if(num==9)
-   {
+
+
+   //remainder
+
+
+  else if(num==9)
+  {
     int x,y;
-    cout<<"Enter a value ";
+    double c;
+    cout<<"Enter a value"<<endl;
     cin>>x;
-    cout<<"Enter another value ";
+    cout<<"Enter another value"<<endl;    
     cin>>y;
     c=x%y;
     cout<<"The result is "<<c<<endl;
-   }
+  }
+
+
    else
-    cout<<"invalid number"<<endl;
+    cout<<"domain error"<<endl;
    cout<<"Do you want to continue?....(y/n)"<<endl;
    cin>>choice;
     }
